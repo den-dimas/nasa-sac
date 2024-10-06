d895ad3eaa14f8461e0afd1221ff0b6c91000ded
Den <dimass.drm@gmail.com>
2024-10-06 08:00:16 +0700

    Change CORS

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 /* Allow Cross Origin Request */
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://osdr.nasa.gov", "http://localhost:3000"],
     credentials: true,
     allowedHeaders: [
       "Authorization",
